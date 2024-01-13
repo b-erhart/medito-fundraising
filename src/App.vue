3f0475c948e8dc5ebfc8761aa8f592e718cc2bf0
Benedikt Erhart <contact@b-erhart.de>
2024-01-13 10:51:50 +0100

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Heading</h1>
+    <p>Hello world!</p>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import 'primevue/resources/themes/lara-dark-blue/theme.css'
+
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(PrimeVue)
+
+app.mount('#app')
